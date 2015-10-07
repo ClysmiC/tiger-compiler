e7fb7c3d1937d02a690600386036c61212b92ecb
ClysmiC <clysmic11@gmail.com>
2015-10-07 11:52:14 -0400

    Added debug flag and output

--- src/com/tiger/compiler/TigerCompiler.java
+++ src/com/tiger/compiler/TigerCompiler.java
@@ -10,10 +10,15 @@ public class TigerCompiler
 {
     public static void main(String[] args)
     {
-        if(args.length == 0)
+        if((args.length != 1 && args.length != 2) || (args.length == 2 && !args[1].equals("-d")))
         {
-            System.out.println("Usage: java TigerCompiler <input-file>");
+            Output.print("Usage: java TigerCompiler <input-file> [-d]");
             System.exit(0);
+        }
+
+        if(args.length == 2 && args[1].equals("-d"))
+        {
+            Output.debugMode = true;
         }
 
         TigerScanner scanner = new TigerScanner(args[0]);
--- src/com/tiger/compiler/frontend/Parser/TigerParser.java
+++ src/com/tiger/compiler/frontend/Parser/TigerParser.java
@@ -1,5 +1,6 @@
 package com.tiger.compiler.frontend.parser;
 
+import com.tiger.compiler.Output;
 import com.tiger.compiler.Tuple;
 import com.tiger.compiler.frontend.scanner.TigerScanner;
 import com.tiger.compiler.frontend.Token;
@@ -31,61 +32,102 @@ public class TigerParser
         Tuple<Token, String> token;
 
         token = tigerScanner.nextToken();
+
+        //print token type
+        if(token.x != Token.ERROR)
+        {
+            Output.println(token.x.toString());
+        }
+        else
+        {
+            Output.println(token.y); //error message
+        }
+
         lookAhead = token.x;
 
         while (true)
         {
-
-            if(token.x == Token.ERROR)
+            if (focus == Token.EOF && lookAhead == Token.EOF)
             {
-                System.out.println("\n" + token.y + "\n");
+                Output.println("\nSuccessful parse");
+                System.exit(0);
             }
-            else
+            else if (focus instanceof Token)
             {
-                if (focus == Token.EOF && lookAhead == Token.EOF)
+                if (focus == lookAhead)
                 {
-                    System.out.println("Successful parse");
-                    System.exit(0);
-                }
-                else if (focus instanceof Token)
-                {
-                    if (focus == lookAhead)
+                    stack.pop();
+                    token = tigerScanner.nextToken();
+                    lookAhead = token.x;
+
+                    //print token type
+                    if(token.x != Token.ERROR)
                     {
-                        stack.pop();
-                        lookAhead = tigerScanner.nextToken().x;
+                        Output.println(token.x.toString());
                     }
                     else
                     {
-                        System.out.println("\nError looking for symbol at top of stack. Parse failed.\nFocus: " + focus + "\nLookahead: " + lookAhead);
-                        System.exit(1);
+                        Output.println("\n" + token.y + "\n"); //error message
                     }
                 }
                 else
                 {
-                    ParserProduction prod = parserTable.lookup(focus, lookAhead);
+                    exitOnFailedParse("Error looking for symbol at top of stack. Parse failed.\nFocus: " + focus + "\nLookahead: " + lookAhead);
+                }
+            }
+            else
+            {
+                ParserProduction prod = parserTable.lookup(focus, lookAhead);
 
-                    if (prod == ParserProduction.ERROR)
-                    {
-                        System.out.println("\nError expanding focus. Parse failed.\nFocus: " + focus + "\nLookahead: " + lookAhead);
-                        System.exit(1);
-                    }
-
-                    System.out.println("\nFocus: " + focus + " Lookahead: " + lookAhead);
-                    System.out.println("Expanding focus: " + prod);
-
-                    stack.pop();
-
-                    List<Enum> rhs = prod.getRhs();
-
-                    for (int i = rhs.size() - 1; i >= 0; i--)
-                    {
-                        if(rhs.get(i) != Token.NULL)
-                            stack.push(rhs.get(i));
-                    }
+                if (prod == ParserProduction.ERROR)
+                {
+                    exitOnFailedParse("Error expanding focus. Parse failed.\n" + "Focus: " + focus + "\nLookahead: " + lookAhead);
                 }
 
-                focus = stack.peek();
+                Output.debugPrintln("\nFocus: " + focus + " Lookahead: " + lookAhead);
+                Output.debugPrintln("Expanding focus: " + prod + "\n");
+
+                stack.pop();
+
+                List<Enum> rhs = prod.getRhs();
+
+                for (int i = rhs.size() - 1; i >= 0; i--)
+                {
+                    if(rhs.get(i) != Token.NULL)
+                        stack.push(rhs.get(i));
+                }
             }
+
+            focus = stack.peek();
         }
+    }
+
+    private void exitOnFailedParse(String debugMessage)
+    {
+
+        //print the rest of the scan. useful for detecting multiple scanner errors
+        while(true)
+        {
+            Tuple<Token, String> token = tigerScanner.nextToken();
+
+            //print token type
+            if(token.x != Token.ERROR)
+            {
+                Output.println(token.x.toString());
+            }
+            else
+            {
+                Output.println("\n" + token.y + "\n"); //error message
+            }
+
+            if(token.x == Token.EOF)
+                break;
+        }
+
+        Output.debugPrintln("\n" + debugMessage);
+
+        Output.println("\nUnsuccessful parse.");
+
+        System.exit(1);
     }
 }
